662c672d74fcd3d31fc232c685e9671a8881efc6
Juliano Kimura <juliano@trianons.com.br>
2022-01-30 16:43:53 -0300

    Primeiro Rascunho do Contrato Soulstone

---
+++ soulstonecontract.sol
@@ -0,0 +1,19 @@
+pragma solidity >0.5.0;
+
+contract Soulstone {
+
+  uint soulDigits = 16;
+  uint soulModulus = 10 ** soulDigits;
+  
+  uint rarityDigits = 1;
+  uint rarityModulus = 100 ** rarityDigits;
+  
+  struct soulstone {
+    string public name
+    
+    
+  
+  }
+
+
+}
